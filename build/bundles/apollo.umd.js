b7984ceb7e20e4b5f2192d70243e1f277f69459f
Jonas Holtorf <Jonas.Holtorf@gmail.com>
2017-09-08 15:41:52 +0200

    encoding

--- build/bundles/apollo.umd.js
+++ build/bundles/apollo.umd.js
@@ -1,13 +1,9 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/core'), require('apollo-client-rxjs'), 
-require('apollo-client'), require('rxjs/observable/from'), require('rxjs/operator/observeOn'), require('rxjs/scheduler/queue'), require('rxjs/Observable')) :
-	typeof define === 'function' && define.amd ? define(['exports', '@angular/core', 'apollo-client-rxjs', 'apollo-client', 'rxjs/observable/from', 
-'rxjs/operator/observeOn', 'rxjs/scheduler/queue', 'rxjs/Observable'], factory) :
-	(factory((global.ng = global.ng || {}, global.ng.apollo = global.ng.apollo || 
-{}),global.ng.core,global.apolloClientRxjs_index,global.apolloClient_index,global.Rx.Observable,global.Rx.Observable.prototype,global.Rx.Scheduler,global.Rx));
-}(this, (function 
-}(exports,_angular_core,apolloClientRxjs_index,apolloClient_index,rxjs_observable_from,rxjs_operator_observeOn,rxjs_scheduler_queue,rxjs_Observable) { 'use 
-}strict';
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/core'), require('apollo-client-rxjs'), require('apollo-client'), require('rxjs/observable/from'), require('rxjs/observable/fromPromise'), require('rxjs/operator/observeOn'), require('rxjs/scheduler/queue')) :
+    typeof define === 'function' && define.amd ? define(['exports', '@angular/core', 'apollo-client-rxjs', 'apollo-client', 'rxjs/observable/from', 'rxjs/observable/fromPromise', 'rxjs/operator/observeOn', 'rxjs/scheduler/queue'], factory) :
+    (factory((global.ng = global.ng || {}, global.ng.apollo = global.ng.apollo || {}),global.ng.core,global.apollo.rxjs,global.apollo,global.Rx.Observable,global.Rx.Observable,global.Rx.Observable.prototype,global.Rx.Scheduler));
+}(this, (function (exports,_angular_core,apolloClientRxjs,apolloClient,rxjs_observable_from,rxjs_observable_fromPromise,rxjs_operator_observeOn,rxjs_scheduler_queue) { 'use strict';
+
 var SelectPipe = (function () {
     function SelectPipe() {
     }
@@ -29,17 +25,18 @@ var SelectPipe = (function () {
             }
         }
     };
-    SelectPipe.decorators = [
-        { type: _angular_core.Pipe, args: [{
-                    name: 'select',
-                },] },
-    ];
-    /**
-     * @nocollapse
-     */
-    SelectPipe.ctorParameters = function () { return []; };
     return SelectPipe;
 }());
+SelectPipe.decorators = [
+    { type: _angular_core.Pipe, args: [{
+                name: 'select',
+            },] },
+];
+/**
+ * @nocollapse
+ */
+SelectPipe.ctorParameters = function () { return []; };
+
 var __extends$1 = (undefined && undefined.__extends) || (function () {
     var extendStatics = Object.setPrototypeOf ||
         ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
@@ -56,32 +53,17 @@ var ApolloQueryObservable = (function (_super) {
         return _super !== null && _super.apply(this, arguments) || this;
     }
     return ApolloQueryObservable;
-}(apolloClientRxjs_index.RxObservableQuery));
-var CLIENT_MAP_WRAPPER = new _angular_core.OpaqueToken('apollo/client-map-wrapper'); var CLIENT_MAP = new _angular_core.OpaqueToken('apollo/client-map'); /**
- * @template T
- * @param {?} promiseFn
- * @return {?}
- */ function fromPromise(promiseFn) {
-    return new rxjs_Observable.Observable(function (subscriber) {
-        promiseFn()
-            .then(function (result) {
-            if (!subscriber.closed) {
-                subscriber.next(result);
-                subscriber.complete();
-            }
-        }, function (error) {
-            if (!subscriber.closed) {
-                subscriber.error(error);
-            }
-        });
-        return function () { return subscriber.unsubscribe(); };
-    });
-}
+}(apolloClientRxjs.RxObservableQuery));
+
+var CLIENT_MAP_WRAPPER = new _angular_core.OpaqueToken('apollo/client-map-wrapper');
+var CLIENT_MAP = new _angular_core.OpaqueToken('apollo/client-map');
+
 /**
  * @template T
  * @param {?} obs
  * @return {?}
- */ function wrapWithZone(obs) {
+ */
+function wrapWithZone(obs) {
     return rxjs_operator_observeOn.observeOn.call(obs, new ZoneScheduler(Zone.current));
 }
 var ZoneScheduler = (function () {
@@ -104,6 +86,7 @@ var ZoneScheduler = (function () {
     };
     return ZoneScheduler;
 }());
+
 var __extends = (undefined && undefined.__extends) || (function () {
     var extendStatics = Object.setPrototypeOf ||
         ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
@@ -116,7 +99,8 @@ var __extends = (undefined && undefined.__extends) || (function () {
 })();
 /**
  * Base class that handles ApolloClient
- */ var ApolloBase = (function () {
+ */
+var ApolloBase = (function () {
     /**
      * @param {?} client
      */
@@ -129,7 +113,7 @@ var __extends = (undefined && undefined.__extends) || (function () {
      * @return {?}
      */
     ApolloBase.prototype.watchQuery = function (options) {
-        return new ApolloQueryObservable(apolloClientRxjs_index.rxify(this.client.watchQuery)(options));
+        return new ApolloQueryObservable(apolloClientRxjs.rxify(this.client.watchQuery)(options));
     };
     /**
      * @template T
@@ -137,8 +121,7 @@ var __extends = (undefined && undefined.__extends) || (function () {
      * @return {?}
      */
     ApolloBase.prototype.query = function (options) {
-        var _this = this;
-        return wrapWithZone(fromPromise(function () { return _this.client.query(options); }));
+        return wrapWithZone(rxjs_observable_fromPromise.fromPromise(this.client.query(options)));
     };
     /**
      * @template T
@@ -146,8 +129,7 @@ var __extends = (undefined && undefined.__extends) || (function () {
      * @return {?}
      */
     ApolloBase.prototype.mutate = function (options) {
-        var _this = this;
-        return wrapWithZone(fromPromise(function () { return _this.client.mutate(options); }));
+        return wrapWithZone((rxjs_observable_fromPromise.fromPromise(this.client.mutate(options))));
     };
     /**
      * @param {?} options
@@ -162,20 +144,21 @@ var __extends = (undefined && undefined.__extends) || (function () {
     ApolloBase.prototype.getClient = function () {
         return this.client;
     };
-    ApolloBase.decorators = [
-        { type: _angular_core.Injectable },
-    ];
-    /**
-     * @nocollapse
-     */
-    ApolloBase.ctorParameters = function () { return [
-        { type: apolloClient_index.ApolloClient, },
-    ]; };
     return ApolloBase;
 }());
+ApolloBase.decorators = [
+    { type: _angular_core.Injectable },
+];
+/**
+ * @nocollapse
+ */
+ApolloBase.ctorParameters = function () { return [
+    { type: apolloClient.ApolloClient, },
+]; };
 /**
  * Container service that works just like the ApolloBase but includes named ApolloClients
- */ var Apollo = (function (_super) {
+ */
+var Apollo = (function (_super) {
     __extends(Apollo, _super);
     /**
      * @param {?} clientMap
@@ -206,24 +189,25 @@ var __extends = (undefined && undefined.__extends) || (function () {
         }
         return this.map.get(name);
     };
-    Apollo.decorators = [
-        { type: _angular_core.Injectable },
-    ];
-    /**
-     * @nocollapse
-     */
-    Apollo.ctorParameters = function () { return [
-        null,
-    ]; };
     return Apollo;
 }(ApolloBase));
+Apollo.decorators = [
+    { type: _angular_core.Injectable },
+];
+/**
+ * @nocollapse
+ */
+Apollo.ctorParameters = function () { return [
+    null,
+]; };
 /**
  * Invokes a ClientMapWrapper
  * @param {?} configWrapper
  * @return {?}
- */ function getClientMap(configWrapper) {
+ */
+function getClientMap(configWrapper) {
     var /** @type {?} */ config = configWrapper();
-    if (config instanceof apolloClient_index.ApolloClient) {
+    if (config instanceof apolloClient.ApolloClient) {
         return { default: config };
     }
     return config;
@@ -232,7 +216,8 @@ var __extends = (undefined && undefined.__extends) || (function () {
  * Provides a value for a map and a wrapper
  * @param {?} configWrapper
  * @return {?}
- */ function provideClientMap(configWrapper) {
+ */
+function provideClientMap(configWrapper) {
     return [{
             provide: CLIENT_MAP_WRAPPER,
             useValue: configWrapper,
@@ -244,7 +229,8 @@ var __extends = (undefined && undefined.__extends) || (function () {
 }
 /**
  * Provides the Apollo service
- */ var provideApollo = {
+ */
+var provideApollo = {
     provide: Apollo,
     useFactory: createApollo,
     deps: [CLIENT_MAP],
@@ -252,15 +238,22 @@ var __extends = (undefined && undefined.__extends) || (function () {
 /**
  * @param {?} clientMap
  * @return {?}
- */ function createApollo(clientMap) {
+ */
+function createApollo(clientMap) {
     return new Apollo(clientMap);
 }
+
 var APOLLO_DIRECTIVES = [
-    SelectPipe, ]; var APOLLO_PROVIDERS = [
-    provideApollo, ]; /**
+    SelectPipe,
+];
+var APOLLO_PROVIDERS = [
+    provideApollo,
+];
+/**
  * @param {?} clientFn
  * @return {?}
- */ function defaultApolloClient(clientFn) {
+ */
+function defaultApolloClient(clientFn) {
     return provideClientMap(clientFn);
 }
 var ApolloModule = (function () {
@@ -293,20 +286,31 @@ var ApolloModule = (function () {
             ],
         };
     };
-    ApolloModule.decorators = [
-        { type: _angular_core.NgModule, args: [{
-                    declarations: APOLLO_DIRECTIVES,
-                    exports: APOLLO_DIRECTIVES,
-                },] },
-    ];
-    /**
-     * @nocollapse
-     */
-    ApolloModule.ctorParameters = function () { return []; };
     return ApolloModule;
 }());
-exports.SelectPipe = SelectPipe; exports.Apollo = Apollo; exports.ApolloBase = ApolloBase; exports.provideClientMap = provideClientMap; 
-exports.ApolloQueryObservable = ApolloQueryObservable; exports.CLIENT_MAP = CLIENT_MAP; exports.CLIENT_MAP_WRAPPER = CLIENT_MAP_WRAPPER; exports.ApolloModule = 
-ApolloModule; exports.APOLLO_PROVIDERS = APOLLO_PROVIDERS; exports.APOLLO_DIRECTIVES = APOLLO_DIRECTIVES; exports.defaultApolloClient = defaultApolloClient; 
+ApolloModule.decorators = [
+    { type: _angular_core.NgModule, args: [{
+                declarations: APOLLO_DIRECTIVES,
+                exports: APOLLO_DIRECTIVES,
+            },] },
+];
+/**
+ * @nocollapse
+ */
+ApolloModule.ctorParameters = function () { return []; };
+
+exports.SelectPipe = SelectPipe;
+exports.Apollo = Apollo;
+exports.ApolloBase = ApolloBase;
+exports.provideClientMap = provideClientMap;
+exports.ApolloQueryObservable = ApolloQueryObservable;
+exports.CLIENT_MAP = CLIENT_MAP;
+exports.CLIENT_MAP_WRAPPER = CLIENT_MAP_WRAPPER;
+exports.ApolloModule = ApolloModule;
+exports.APOLLO_PROVIDERS = APOLLO_PROVIDERS;
+exports.APOLLO_DIRECTIVES = APOLLO_DIRECTIVES;
+exports.defaultApolloClient = defaultApolloClient;
+
 Object.defineProperty(exports, '__esModule', { value: true });
+
 })));
